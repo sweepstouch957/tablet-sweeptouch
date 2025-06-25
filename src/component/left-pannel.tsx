f7fb736ca7fc31f183650bf52ca2b245685cf636
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-06-25 17:26:34 -0600

    update ui

--- src/component/left-pannel.tsx
+++ src/component/left-pannel.tsx
@@ -63,16 +63,15 @@ const LeftPanel: React.FC<LeftPanelProps> = ({
   }) {
     const date = new Date().toLocaleString();
 
-    const ticket = `========================================
+    const ticket = `=============================
 ${data.storeName.toUpperCase()}
-========================================
+=============================
 PHONE   : ${data.phone}
 COUPON  : ${data.couponCode}
 DATE    : ${date}
-----------------------------------------
+-----------------------------
 THANK YOU FOR PARTICIPATING
-PLEASE KEEP THIS RECEIPT
-========================================`;
+PLEASE KEEP THIS RECEIPT`;
 
     const encoded = encodeURIComponent(ticket);
     window.location.href = `rawbt:${encoded}`;
