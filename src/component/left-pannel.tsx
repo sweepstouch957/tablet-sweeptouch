3a73d6d4def04dd7e67b4ef0c8a68bb1b17a4ce5
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-07-09 15:58:31 -0600

    fix all

--- src/component/left-pannel.tsx
+++ src/component/left-pannel.tsx
@@ -73,8 +73,8 @@ const LeftPanel: React.FC<LeftPanelProps> = ({
         <Image
           src={optinType === "generic" ? VipImage.src : NewYearImage.src}
           alt="New Year 2026"
-          width={200}
-          height={150}
+          width={150}
+          height={80}
           style={{
             objectFit: "contain",
             width: matches && optinType === "generic" ? "80%" : "100%",
