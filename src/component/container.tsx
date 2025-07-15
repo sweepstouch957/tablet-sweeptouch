647213b3a65d5ce807fbcff9ccaf6856754dc682
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-07-15 08:26:40 -0600

    delete

--- src/component/container.tsx
+++ src/component/container.tsx
@@ -4,8 +4,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { useState, type FC, type ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/context/auth-context";
-import { Offline } from "react-detect-offline";
-import NoInternet from "./NoInternet";
 
 interface LayoutProps {
   children: ReactNode;
@@ -19,9 +17,6 @@ export const Layout: FC<LayoutProps> = (props: LayoutProps) => {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <CssBaseline />
-        <Offline>
-          <NoInternet />
-        </Offline>
         {children}
       </AuthProvider>
     </QueryClientProvider>
