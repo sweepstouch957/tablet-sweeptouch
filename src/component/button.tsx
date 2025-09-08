a22a62f698cbc22f04cdd70674426dedb0747a4f
Jared Palma <jared@sweepstouch.com>
2025-09-08 16:28:08 -0600

    cambios en animaciones

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -5,8 +5,7 @@ import {
   useTheme,
   useMediaQuery,
 } from "@mui/material";
-import Image from "next/image";
-import TapIcon from "@public/Click.webp"; // Asegurate que esta ruta esté bien
+//import Image from "next/image";
 
 export default function CallToActionButton({
   onClick,
@@ -14,7 +13,7 @@ export default function CallToActionButton({
   onClick: () => void;
 }) {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  //const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
     <Box
@@ -31,7 +30,7 @@ export default function CallToActionButton({
           borderRadius: "32px",
           px: 2,
           py: 1.5,
-          width:"100%",
+          width: "100%",
           textTransform: "uppercase",
           position: "relative",
           zIndex: 1,
@@ -45,47 +44,6 @@ export default function CallToActionButton({
         </Typography>
       </Button>
 
-      <Box
-        sx={{
-          animation: isMobile
-            ? "shakeX 2s ease-in-out infinite"
-            : "shakeY 1.5s ease-in-out infinite",
-            position:"absolute",
-            zIndex:1,
-            right:1,
-            top:1
-        }}
-      >
-        <Image
-          src={TapIcon}
-          alt="Click icon"
-          width={30}
-          height={30}
-          style={{ objectFit: "contain" }}
-        />
-      </Box>
-
-      <style jsx global>{`
-        @keyframes shakeX {
-          0%,
-          100% {
-            transform: translateX(0);
-          }
-          50% {
-            transform: translateX(60px);
-          }
-        }
-
-        @keyframes shakeY {
-          0%,
-          100% {
-            transform: translateY(0);
-          }
-          50% {
-            transform: translateY(140px);
-          }
-        }
-      `}</style>
     </Box>
   );
 }
